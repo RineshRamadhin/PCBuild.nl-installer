03777234f521d89657a64c403e824e39e627ec61
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2015-04-07 20:54:44 +0200

    First commit
    pcbuild.nl installer
    v0.1

---
+++ PCBuild.nl Installer/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PCBuild.nl_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists(@"C:\PCBuild.nl\"))
+                Directory.CreateDirectory(@"C:\PCBuild.nl\");
+        }
+
+        /// <summary>
+        /// opent form Download_neo4j
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Download_neo4j f1 = new Download_neo4j();
+            f1.Show();
+        }
+    }
+}
